96e602d764e0d6ea075a83b994bb9b9160236cc9
AsAarthi <140336578+AsAarthi@users.noreply.github.com>
2023-12-31 20:28:29 +0530

    Update History.component.jsx

--- components/History/History.component.jsx
+++ components/History/History.component.jsx
@@ -1,31 +1,52 @@
 import { useState } from "react";
-import { MdModeEdit } from "react-icons/md";
-import { TbHttpDelete } from "react-icons/tb";
 import { Data } from "../../constants";
 import Heading from "../Heading/Heading.component";
+import Task from "../Task/Task.component";
+import { Outlet } from "react-router-dom";
+import { MdModeEdit } from "react-icons/md";
+import { TbHttpDelete } from "react-icons/tb";
 
-const History = ({
-    books,
-    deleteHandler,
-    editOptionHandler
-  }) => {
-const updateHandler = (id)=>{
-  editOptionHandler(id)
+const History = () => {
+  <Outlet/>
+  const [books, setBooks] = useState(Data);
+  const [isTaskOpen, setIsTaskOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [bookEdit, setIsBookEdit] = useState("");
 
-}
-const handleDelete = (id)=>{
-  deleteHandler(id)
-}
+  const editOptionHandler = (id) => {
+    setIsTaskOpen(true);
+    books.map((book) => {
+      if (book.id === id) {
+        setIsBookEdit(book);
+      }
+    });
+  };
+
+  const editHandler = (id, updatedTitle, updatedAuthor) => {
+    const updatedBook = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: updatedTitle, author: updatedAuthor };
+      }
+      return book;
+    });
+    setBooks(updatedBook);
+  };
+
+  const deleteHandler = (id) => {
+    const remainBook = books.filter((book) => book.id !== id);
+    setBooks(remainBook);
+    // console.log(remainBook);
+  };
+
   return (
     <>
-        <Heading/>
-    {books.map((book) => {
-      console.log(book);
-      console.log(book.class);
-    if(book.class === `History`)
-    {
-    return(
-    <main
+      <Heading />
+      {books.map((book) => {
+        // console.log(book);
+        if(book.class==='History')
+        {
+          return (
+      <main
       className="flex justify-start items-center my-[2rem] px-[1rem]"
       key={book.id}
     >
@@ -41,17 +62,25 @@ const handleDelete = (id)=>{
       <div className="flex justify-evenly w-[30%] mx-auto cursor-pointer gap-40">
         <MdModeEdit
           className="text-[35px] text-green-500"
-          onClick={()=> updateHandler(book.id)}
+          onClick={()=> editOptionHandler(book.id)}
         />
         <TbHttpDelete
           className="text-[35px] text-red-600"
-          onClick={()=> handleDelete(book.id)}
+          onClick={()=> deleteHandler(book.id)}
         />
       </div>
     </main>
-  )}
+    );}
   })}
-  </>
+    <Task
+        isTaskOpen={isTaskOpen}
+        setIsTaskOpen={setIsTaskOpen}
+        isEditing={isEditing}
+        editHandler={editHandler}
+        deleteHandler={deleteHandler}
+        bookEdit={bookEdit}
+      />
+    </>
   );
 };
 
